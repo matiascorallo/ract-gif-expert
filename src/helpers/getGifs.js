ce167f386493f84d0a4663c18c1b92236e6e296e
Matias <maticorallo13579@gmail.com>
2022-09-15 20:34:59 -0300

    primer commmit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async( category ) => { 
+          
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=j12jT1zkzL1rCtpNCYYbc51jzzs2NDIg&q=${ category }&limit=10`
+    const resp = await fetch(url);
+    const {data} = await resp.json()
+  
+    const gifs = data.map (img => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url
+  
+    }));
+  
+    return gifs;
+  }
